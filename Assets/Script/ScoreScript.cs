001deb98da7d38829f0b695128f9b13d1cabab65
Azerrr <None>
2021-07-25 09:38:50 +0200

    Remove score text attribute

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -5,14 +5,12 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-	private Text scoreText;
-
     // Start is called before the first frame update
     void Start()
     {
         int score = 10; //TODO
         int bestScore = 100; //TODO
-        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        Text scoreText = GameObject.Find("Score").GetComponent<Text>();
         scoreText.text = "Score: " + score + "\nBest: " + bestScore;
     }
 
