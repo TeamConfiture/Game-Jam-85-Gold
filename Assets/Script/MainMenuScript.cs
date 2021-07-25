d27c9354cb405e08182fb4f27b43d0118c71790b
Azerrr <None>
2021-07-25 12:03:38 +0200

    Add rules

--- Assets/Script/MainMenuScript.cs
+++ Assets/Script/MainMenuScript.cs
@@ -1,41 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject CreditsMenu;
-    public GameObject Rules1Menu;
+    public GameObject RulesMenu;
+    public Image RulesImage;
+    private static int ruleStep = 1;
 
-    public void PlayNowButton(string scene) {
+    public void Start() {
+    }
+    public void PlayButton(string scene) {
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
     }
-    public void Rules1Button()
+    public void RulesButton()
     {
-        Rules1Menu.SetActive(true);
-        CreditsMenu.SetActive(false);
+        RulesMenu.SetActive(true);
         MainMenu.SetActive(false);
     }
-
-
     public void CreditsButton() {
         CreditsMenu.SetActive(true);
         MainMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
     }
     public void MainMenuButton() {
         MainMenu.SetActive(true);
         CreditsMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
+        RulesMenu.SetActive(false);
     }
-    public void BackButton(){
-        MainMenu.SetActive(true);
-        CreditsMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
+    public void BackButton() {
+        RulesMenu.transform.Find("NextButton").gameObject.SetActive(true);
+        RulesMenu.transform.Find("PlayButton").gameObject.SetActive(false);
+        if (ruleStep == 1) {
+            MainMenuButton();
+        } else {
+            ruleStep -= 1;
+            loadRules();
+        }
     }
     public void QuitButton() {
         Application.Quit();
+    }
+    public void NextButton() {
+        ruleStep += 1;
+        loadRules();
+        // Last step
+        if (ruleStep >= 4) {
+            RulesMenu.transform.Find("NextButton").gameObject.SetActive(false);
+            RulesMenu.transform.Find("PlayButton").gameObject.SetActive(true);
+        }
+    }
+
+    private void loadRules() {
+        RulesImage.sprite = Resources.Load<Sprite>("Rules/Rules" + ruleStep);
     }
     
 }
