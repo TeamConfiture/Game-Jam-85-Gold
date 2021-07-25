a229d4fe0e99243d8a8b755edfd2eae6d4afa15e
Azerrr <None>
2021-07-25 12:03:38 +0200

    WIP: test rules

--- Assets/Script/MainMenuScript.cs
+++ Assets/Script/MainMenuScript.cs
@@ -1,41 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject CreditsMenu;
-    public GameObject Rules1Menu;
+    public GameObject RulesMenu;
+    public Image RulesImage;
+    private int ruleStep = 1;
 
-    public void PlayNowButton(string scene) {
+    public void Start() {
+        RulesImage = GetComponent<Image>();
+    }
+    public void PlayButton(string scene) {
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
     }
-    public void Rules1Button()
+    public void RulesButton()
     {
-        Rules1Menu.SetActive(true);
-        CreditsMenu.SetActive(false);
+        ruleStep = 1;
+        RulesImage.sprite = Resources.Load<Sprite>("Rules" + ruleStep);
+        // RulesMenu.Find("NextButton").SetActive(true);
+        // RulesMenu.Find("PlayButton").SetActive(false);
+        RulesMenu.SetActive(true);
+        // CreditsMenu.SetActive(false);
         MainMenu.SetActive(false);
     }
-
-
     public void CreditsButton() {
         CreditsMenu.SetActive(true);
         MainMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
+        // RulesMenu.SetActive(false);
     }
     public void MainMenuButton() {
         MainMenu.SetActive(true);
         CreditsMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
+        RulesMenu.SetActive(false);
     }
     public void BackButton(){
         MainMenu.SetActive(true);
         CreditsMenu.SetActive(false);
-        Rules1Menu.SetActive(false);
+        RulesMenu.SetActive(false);
     }
     public void QuitButton() {
         Application.Quit();
+    }
+    public void NextButton() {
+        ruleStep += 1;
+        // ruleImage = Resources.Load<Sprite>("Rules/Rules" + ruleStep);
+        // Last step
+        if (ruleStep >= 4) {
+            // RulesMenu.Find("NextButton").SetActive(false);
+            // RulesMenu.Find("PlayButton").SetActive(true);
+        }
     }
     
 }
